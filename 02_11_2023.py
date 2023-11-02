99988efaf44084fce018d676afc8383851e7a09e
viveksharmar <146196605+viveksharmar@users.noreply.github.com>
2023-11-02 22:30:47 +0530

    Add files via upload

---
+++ 02_11_2023.py
@@ -0,0 +1,31 @@
+# PYTHON PROJECTS
+# Remote(not in physical touch) Code Execution(RCE)
+# IP address in India== 192.168.10.200
+# In 4 segments--- every segment has range 0-255--it cannot exceed or preceed it in India
+# IP is used for connectivity of systems(to make network)
+# PORT number -- IP address is address of system but system may use many programs-- Port number specifically gives a program of that ip
+# Prot no. range--> 0-65536
+
+# Protocol - TCP & UDP
+# Protocol is set of rules/instructions followed in communication b/w two or more systems
+# TCP (connection oriented) -- proper connectivity and connection from both at the same time is required(Eg-phone call)
+# UDP(connection independent) -- No connection required at the same time(Eg-whatsapp msg)
+
+# Socket Programming(whatsapp-- send data from one device to another)-- network/internet needed
+
+# import os
+# del os
+# del is the keyword used to unimport/delete any module
+# to import particular part of any module-- 
+# from time import ctime
+# del ctime
+# part of module will be deleted(del time will raise error as the time is not imported)
+# alias-- false name or nickname creation
+# import time as t
+
+# encoding and encryption while sending message
+
+# socket --- IP:PORT --- 157.240.239.35:443
+# This is for facebook
+
+# backdoor software in hacking
